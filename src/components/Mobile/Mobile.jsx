e21df4f5c1aa983327dbbb9e43665987775a791a
kaung <kaungmyathun7@gmail.com>
2023-07-12 16:04:01 +0630

    update

--- src/components/Mobile/Mobile.jsx
+++ src/components/Mobile/Mobile.jsx
@@ -7,7 +7,7 @@ function Mobile(props) {
     <div
       className={`bg-white text-center ${style.mainContainer} display-flex justify-center items-center text-black`}>
       <h1 className={`text-4xl`}>
-        You are using <span className={`text-blue-700`}>{props.name.name}</span> device.And width is <span className={`text-blue-700`}>{props.width}
+        You are using <span className={`text-blue-700`}>{props.name.name}</span> device. <br /> And width is <span className={`text-blue-700`}>{props.width}
       </span></h1>
       <p className={`text-2xl`}>Please Use computer (or) laptop to see my portfolio</p>
       <p className={`text-2xl`}>Thanks</p>
